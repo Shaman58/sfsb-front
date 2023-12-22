f1439d5090cb0a631d9d2af582b7d816cf6065c2
voskanvl <voskanvl@gmail.com>
2023-12-22 20:05:58 +0300

    route guard to a supplier

--- sfsb/src/router/index.ts
+++ sfsb/src/router/index.ts
@@ -4,6 +4,10 @@ import Technology from "@/views/Technology.vue";
 import MainPage from "@/views/MainPage.vue";
 import Staff from "@/views/Staff.vue";
 import Supplier from "@/views/Supplier.vue";
+import { useCurrentUserStore } from "@/pinia-store/currentUser";
+import { useToast } from "vue-toast-notification";
+
+const toast = useToast();
 
 const routes = [
     {
@@ -13,6 +17,9 @@ const routes = [
     {
         path: "/supplier",
         component: Supplier,
+        meta: {
+            onlyFor: "supplier",
+        },
     },
     {
         path: "/staff",
@@ -31,6 +38,22 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.path === "/supplier") {
+        const { user } = useCurrentUserStore();
+        if (
+            user &&
+            user.roles.map((e) => e.toLowerCase()).includes("supplier")
+        ) {
+            return next();
+        } else {
+            toast.error("У вас нет прав доступа к этой странице");
+            return next({ path: "/" });
+        }
+    }
+    next();
 });
 
 export default router;
