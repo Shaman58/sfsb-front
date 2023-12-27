1208e7792d99df9ffefd178139f10d4b05e0c56b
voskanvl <voskanvl@gmail.com>
2023-12-27 13:38:23 +0300

    fix cardowner

--- sfsb/src/components/technology/TechnologyCardOwner.vue
+++ sfsb/src/components/technology/TechnologyCardOwner.vue
@@ -3,27 +3,27 @@
     .technology-card__owner-title
         span(v-if="technologyUser") Последние изменения внесены: {{ technologyUser.firstName }} {{technologyUser.lastName }}
         span(v-else="technologyUser" ) Данные не были внесены
-    v-switch.technology-card__owner-switcher(v-model="model" :label="model?'В работе':'Взять в работу'" color="red" :untouchable="currentItem.technology.blocked!=='' || !isBlockedByCurrentUser")
+    .technology-card__owner-switcher(:untouchable="untouchable")
+        v-switch(v-model="model" :label="model?'В работе':'Взять в работу'" color="red" )
 
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { computed, ref, watch } from 'vue';
 import { useTechnologyStore } from "@/pinia-store/technology"
-import { useCurrentUserStore } from '@/pinia-store/currentUser';
 import { storeToRefs } from 'pinia';
 
 const emit = defineEmits(["change"])
 
 const { isBlockedByCurrentUser, currentItem } = storeToRefs(useTechnologyStore())
 const { changeBlocked } = useTechnologyStore()
-const { user: currentUser } = storeToRefs(useCurrentUserStore())
 
 const model = ref(isBlockedByCurrentUser.value)
 
 const technologyUser = currentItem.value.technology.user
 
-// model.value = user.id === currentUser.value?.id
+const untouchable = computed(() => !!currentItem.value.technology.blocked && !isBlockedByCurrentUser.value)
+
 model.value = isBlockedByCurrentUser.value
 
 watch(() => model.value, async (newVal: boolean) => {
