519458e675a1e5ac9fec863325d659ecce859168
voskanvl <voskanvl@gmail.com>
2024-01-23 19:43:23 +0300

    DataPageTabs continue2

---
+++ sfsb/src/components/data-page/Material.vue
@@ -0,0 +1,80 @@
+<template lang="pug">
+    v-form.material-form(ref="form" v-if="materialLocal" )
+        v-card.material-form__card
+            v-card-title.material-form__title {{ materialLocal.materialName}}
+            v-card-text.material-form__controls
+                v-row
+                    v-col(cols="4")
+                        v-text-field(label="Название:" v-model="materialLocal.materialName" :rules="[rules.required, rules.nameValidation]" counter :disabled="route.path !== '/date'")
+                    v-col(cols="4")
+                        v-select(label="Выберите вид:" :items="geometries" item-title="title" item-value="label"
+                            v-model="materialLocal.geometry" :rules="[rules.required]" :disabled="route.path !== '/date'")
+                    v-col(cols="4")
+                        v-text-field(label="Гост на метериал:" v-model="materialLocal.gost1" :disabled="route.path !== '/date'")
+                v-row
+                    v-col(cols="4")
+                        v-select(label="Выберите плотность:" :items="templates" item-title="materialTypeName"
+                            item-value="density" v-model="materialLocal.density" :disabled="route.path !== '/date'")
+                    v-col(cols="2")
+                        v-text-field(label="Плотность:" v-model="materialLocal.density"
+                            :rules="[rules.required, rules.numeric]" :disabled="route.path !== '/date'")
+                    v-col(cols="2")
+                        v-text-field(label="Стоимость килограмма:" v-model="materialLocal.price.amount"
+                            :rules="[rules.required, rules.numeric]" type="number"
+                            :disabled="route.path !== '/date' && route.path !== '/supplier'")
+                    v-col(cols="4")
+                        v-text-field(label="Гост на сортамент:" v-model="materialLocal.gost2"
+                            :disabled="route.path !== '/date'")
+
+            v-card-actions.material-form__actions
+                v-btn.material-form__btn(@click="save") Изменить
+
+</template>
+
+<script setup lang="ts">
+import {onMounted, type Ref, ref, toRefs, toValue} from "vue";
+import {useValidationRules} from "@/mixins/FieldValidationRules";
+import {useRoute} from "vue-router";
+import CONST from "@/consts";
+import {storeToRefs} from "pinia";
+import {useMaterialTemplatesStore} from "@/pinia-store/materialTemplates";
+import {useMaterialsStore} from "@/pinia-store/materials";
+import Material from "@/components/data-page/Material.vue";
+
+const {materialTemplates: templates} = storeToRefs(useMaterialTemplatesStore())
+const {fetchMaterialTemplates} = useMaterialTemplatesStore()
+const {saveMaterial} = useMaterialsStore()
+
+const props = defineProps<{ material: Material }>()
+const {material} = toRefs(props)
+const form = ref<HTMLFormElement>()
+const route = useRoute()
+
+const {rules} = useValidationRules();
+const geometries = CONST.GEOMETRIES
+const materialLocal: Ref<Material> = ref(material)
+
+// const isDiffPropsLocal: ComputedRef<boolean> = computed(() => {
+//     if (!materialLocal || !materialLocal.value || !material.value) return false
+//     return (Object.keys(material.value) as Array<keyof typeof material.value>)
+//         .every((key: keyof typeof material.value) => material.value[key] === materialLocal.value[key]
+//         )
+// })
+
+const save = async () => {
+    if (!materialLocal.value) return
+    await saveMaterial(material.value)
+}
+onMounted(async () => {
+    !templates.value.length && await fetchMaterialTemplates()
+    materialLocal.value = toValue(material) as Material
+
+})
+
+
+</script>
+
+
+<style scoped lang="sass">
+
+</style>
--- sfsb/src/views/DataPageTabs.vue
+++ sfsb/src/views/DataPageTabs.vue
@@ -9,25 +9,38 @@
                         v-text-field(label="фильтр")
                     v-card-text
                         v-list.datapage-tabs__list
-                            v-list-item(v-for="item in normalizedList" :key="item.id" @click="currentTool=item") {{ item.name }}
-                v-card.datapage-tabs__card
-                    .datapage-tabs__card-material(v-if="currentTab.type==='Material'")
-                    .datapage-tabs__card-tool(v-else)
+                            v-list-item.datapage-tabs__list-item(v-for="item in normalizedList" :key="item.id" @click="setCurrentTool(item)")
+                                .datapage-tabs__list-name {{ item.name }}
+                                .datapage-tabs__list-options
+                                    .datapage-tabs__list-option(v-if="item.gost1") {{ item.gost1}}
+                                    .datapage-tabs__list-option(v-if="item.gost2") {{ item.gost2}}
+                                    .datapage-tabs__list-option(v-if="item.description") {{ item.description}}
+                .datapage-tabs__card
+                    .datapage-tabs__card-content(v-if="currentTab?.type==='Material'")
+                        | MATERIAL
+                        | {{ isReactive(currentTool) ? "REACTIVE" : "NOT REACTIVE" }}
+                        MaterialComponent(:material="currentTool" )
+                    .datapage-tabs__card-content(v-if="currentTab?.type==='Tool'")
+                        //ToolComponent(:tool="currentTool" )
+                        | TOOL
 </template>
 
 <script setup lang="ts">
 
-import {computed, type MaybeRef, onMounted, type Ref, ref, toValue} from "vue";
+import {computed, isReactive, onMounted, type Ref, ref, toRef, toValue} from "vue";
 import {storeToRefs} from "pinia";
 import {useMaterialsStore} from "@/pinia-store/materials";
 import {useToolingStore} from "@/pinia-store/tooling";
 import {useCuttersStore} from "@/pinia-store/cutters";
 import {useSpecialStore} from "@/pinia-store/specials";
+import MaterialComponent from "@/components/data-page/Material.vue";
+
+type MaterialOrTool = Material | Tool
 
 interface SwitchTab {
     id: number
     name: string,
-    list: MaybeRef<Material[] | Tool[]>
+    list: Ref<Material[]> | Ref<Tool[]>
     type: "Tool" | "Material"
 }
 
@@ -41,23 +54,16 @@ const {fetchToolings} = useToolingStore()
 const {fetchCutters} = useCuttersStore()
 const {fetchSpecials} = useSpecialStore()
 
-onMounted(async () => {
-    !materials.value.length && await fetchMaterials()
-    !toolings.value.length && await fetchToolings()
-    !cutters.value.length && await fetchCutters()
-    !specials.value.length && await fetchSpecials()
-})
-
 
 const switches: Readonly<SwitchTab[]> = [
-    {id: 1, name: "Материалы", list: materials, type: "Tool"},
+    {id: 1, name: "Материалы", list: materials, type: "Material"},
     {id: 2, name: "Инструменты", list: cutters, type: "Tool"},
     {id: 3, name: "Специнструменты", list: specials, type: "Tool"},
     {id: 4, name: "Остастка", list: toolings, type: "Tool"},
 ] as const
 
-const currentTab: Ref<SwitchTab> = ref(switches[0])
-const currentTool: Ref<Material | Tool | undefined> = ref(toValue(currentTab.value.list).at(0))
+const currentTab = toRef(switches[0])
+const currentTool: Ref<Material> | Ref<Tool> = ref(toValue(currentTab.value.list).at(0) as Material)
 const list = ref([])
 
 const normalizedList = computed(() => toValue(currentTab.value.list).map((e: Material | Tool) => {
@@ -73,8 +79,22 @@ const normalizedList = computed(() => toValue(currentTab.value.list).map((e: Mat
 
 const switchTab = (item: SwitchTab) => {
     currentTab.value = toValue(item)
-    currentTool.value = toValue(currentTab.value.list).at(0)
+    currentTool.value = normalizedList.value.at(0)
 }
+
+const setCurrentTool = (item: typeof normalizedList.value[0]) => {
+    if (!item) return
+    currentTool.value = currentTab.value.list.find(e => e.id === item.id)
+}
+
+onMounted(async () => {
+    !materials.value.length && await fetchMaterials()
+    !toolings.value.length && await fetchToolings()
+    !cutters.value.length && await fetchCutters()
+    !specials.value.length && await fetchSpecials()
+
+    currentTool.value = toValue(currentTab.value.list).at(0) as Material
+})
 </script>
 
 
@@ -101,5 +121,20 @@ const switchTab = (item: SwitchTab) => {
         display: grid
         grid-template-columns: 1fr 4fr
         gap: .5rem
+
+    &__list-name
+        font-size: 1.1rem
+        font-weight: 700
+
+    &__list-options
+        margin-top: 0.5rem
+        display: flex
+        gap: .5rem
+        flex-wrap: wrap
+
+    &__list-option
+        font-weight: 200
+        font-size: 0.9rem
+
 
 </style>
