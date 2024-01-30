e2bf3419af084de63b25e8bf6c0f59bfd85f7079
voskanvl <voskanvl@gmail.com>
2024-01-30 17:12:51 +0300

    remove Switches pinia

--- sfsb/src/pinia-store/tools.ts
+++ sfsb/src/pinia-store/tools.ts
@@ -1,6 +1,18 @@
 import {defineStore} from "pinia";
 import ToolStore from "@/pinia-store/ToolStore";
+import {computed, Ref} from "vue";
 
+interface SwitchTab {
+    id: number;
+    path: string;
+    name: string;
+    list: Material[] | Tool[];
+    type: "Tool" | "Material";
+    save: (item: any) => Promise<void>;
+    fetch: () => Promise<void>;
+    newData: () => Promise<void>;
+    loading: Ref<boolean>
+}
 
 export const useToolingStore
     = defineStore("tooling", () => new ToolStore<Tool>("/tooling"));
@@ -19,3 +31,51 @@ export type SpecialStoreType = typeof useSpecialStore
 export const useMaterialsStore
     = defineStore("materials", () => new ToolStore<Material>("/material"))
 export type MaterialsStoreType = typeof useMaterialsStore
+
+export const useSwitches = () => computed(() => [
+    {
+        id: 1,
+        path: "/data/materials",
+        name: "Материалы",
+        list: useMaterialsStore().tools,
+        type: "Material",
+        save: useMaterialsStore().saveTool,
+        fetch: useMaterialsStore().fetchTool,
+        newData: useMaterialsStore().newData,
+        loading: useMaterialsStore().loading
+    },
+    {
+        id: 2,
+        path: "/data/cutters",
+        name: "Инструменты",
+        list: useCuttersStore().tools,
+        type: "Tool",
+        save: useCuttersStore().saveTool,
+        fetch: useCuttersStore().fetchTool,
+        newData: useCuttersStore().newData,
+        loading: useCuttersStore().loading
+    },
+    {
+        id: 3,
+        path: "/data/specials",
+        name: "Специнструменты",
+        list: useSpecialStore().tools,
+        type: "Tool",
+        save: useSpecialStore().saveTool,
+        fetch: useSpecialStore().fetchTool,
+        newData: useSpecialStore().newData,
+        loading: useSpecialStore().loading
+    },
+    {
+        id: 4,
+        path: "/data/toolings",
+        name: "Остастка",
+        list: useToolingStore().tools,
+        type: "Tool",
+        save: useToolingStore().saveTool,
+        fetch: useToolingStore().fetchTool,
+        newData: useToolingStore().newData,
+        loading: useToolingStore().loading
+    },
+])
+
