a6e91d668a9a2a284e69c9b5485b9b6cb561e5f8
voskanvl <voskanvl@gmail.com>
2024-02-06 07:21:25 +0300

    Operation refactoring

--- sfsb/src/components/commerce/Operations/OperationsComponent.vue
+++ sfsb/src/components/commerce/Operations/OperationsComponent.vue
@@ -4,51 +4,22 @@
         template(#filter)
             v-text-field(label="фильтр" v-model="filterText")
         template(#list)
-            v-list
-                v-list-item
-                    v-list-item-title
-                        router-link.list-link(:to="`/commerce/operations/new`")
-                            span(:style="{color: 'orange'}") Добавить новую операцию
-                v-list-item(v-for="i in operations" @click="currentOperation=i"
-                    :key="i.id" :active="+page===i.id")
-                    v-list-item-title
-                        router-link.list-link(:to="`/commerce/operations/${i.id}`") {{i.operationName}}
+            suspended-component
+                OperationsList(:filter="filterText")
         template(#card)
-            router-view
+            suspended-component
+                router-view
             //ClientCard(:company="currentCompany" :loading)
 
 </template>
 <script setup lang="ts">
-import {storeToRefs} from "pinia";
-import {computed, onMounted, ref, toRefs, watchEffect} from "vue";
+import {ref} from "vue";
 import LayoutPage from "@/components/common/LayoutPage.vue";
-import {useRoute, useRouter} from "vue-router";
-import {useOperationsStore} from "@/pinia-store/operations";
-
-const router = useRouter()
-
-const route = useRoute();
-const {path} = toRefs(route);
-const page = computed(() => path.value.split("/").at(-1))
-
-
-const {operations, loading} = storeToRefs(useOperationsStore())
-const {fetchOperation} = useOperationsStore()
-
+import SuspendedComponent from "@/components/common/SuspendedComponent.vue";
+import OperationsList from "@/components/commerce/Operations/OperationsList.vue";
 
 const filterText = ref("")
-const currentOperation = ref<Operation>(operations.value[0])
 
-onMounted(async () => {
-    !operations.value.length && await fetchOperation()
-    const firstId = computed(() => operations.value.length && operations.value[0].id)
-    page.value === "operations" && await router.push(`/commerce/operations/${firstId.value}`)
-
-})
-
-watchEffect(() => {
-    console.log("loading", loading)
-})
 </script>
 <style lang="sass">
 .list-link
---
+++ sfsb/src/components/commerce/Operations/OperationsList.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+    v-list
+        v-list-item
+            v-list-item-title
+                router-link.list-link(:to="`/commerce/operations/new`")
+                    span(:style="{color: 'orange'}") Добавить новую операцию
+        v-list-item(v-for="i in operations" @click="currentOperation=i"
+            :key="i.id" :active="+page===i.id")
+            v-list-item-title
+                router-link.list-link(:to="`/commerce/operations/${i.id}`") {{i.operationName}}
+</template>
+<script setup lang="ts">
+import {storeToRefs} from "pinia";
+import {useOperationsStore} from "@/pinia-store/operations";
+import {computed, ref, toRefs} from "vue";
+import {useRoute, useRouter} from "vue-router";
+
+const router = useRouter()
+
+const route = useRoute();
+const {path} = toRefs(route);
+const page = computed(() => path.value.split("/").at(-1))
+
+const {operations, loading} = storeToRefs(useOperationsStore())
+const {fetchOperation} = useOperationsStore()
+
+const currentOperation = ref<Operation>(operations.value[0])
+
+!operations.value.length && await fetchOperation()
+const firstId = computed(() => operations.value.length && operations.value[0].id)
+page.value === "operations" && await router.push(`/commerce/operations/${firstId.value}`)
+</script>
+
+
+<style scoped lang="sass">
+
+</style>
