896c1fa3742938303a4990851deabd26b3112fa6
voskanvl <voskanvl@gmail.com>
2024-02-10 00:24:37 +0300

    SupplyComponent background

--- sfsb/src/components/supply-new/SupplyComponent.vue
+++ sfsb/src/components/supply-new/SupplyComponent.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
-    LayoutPage.supply(:background="'#ccc'")
+    LayoutPage.supply(:background="'linear-gradient(45deg, #bbb, #efefef)'")
         template(#title) Снабжение
         template(#list)
             v-list
-                v-list-item(@click="()=>{}" v-for="i in supplyMap(useSupplyStore)" :key="i.title")
+                v-list-item(@click="()=>{}" v-for="i in supplyMap(useSupplyStore)" :key="i.title" :active="i.path===path")
                     router-link.list-link(:to="i.path") {{ i.title }}
 
         template(#card)
@@ -13,12 +13,14 @@
 <script setup lang="ts">
 
 import SuspendedComponent from "@/components/common/SuspendedComponent.vue"
-import {ref} from "vue";
+import {ref, toRefs} from "vue";
 import LayoutPage from "@/components/common/LayoutPage.vue";
 import supplyMap from "./SupplyMap";
 import {useSupplyStore} from "@/pinia-store/supply";
+import {useRoute} from "vue-router";
 
 const filterText = ref("")
+const {path} = toRefs(useRoute())
 </script>
 <style scoped lang="sass">
 
