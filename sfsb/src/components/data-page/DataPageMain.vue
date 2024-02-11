073651b66e54d604196e1e8f3103e5accf65427d
voskanvl <voskanvl@gmail.com>
2024-02-11 22:21:40 +0300

    DataPageMaterialItem.vue switch theme

--- sfsb/src/components/data-page/DataPageMain.vue
+++ sfsb/src/components/data-page/DataPageMain.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
-    layout-page
-        template(#title) {{ currentTab?.name }}
-        template(#filter)
-            v-text-field.datapage-main__list-filter(label="фильтр" v-model="filterText")
-        template(#list)
-            data-page-list(:list="selectedList" @select="setCurrentTool" @intersected="onIntersect")
-        template(#card)
-            component(:is="selectedComponent" :item="currentTool" @save="save")
+	layout-page
+		template(#title) {{ currentTab?.name }}
+		template(#filter)
+			v-text-field.datapage-main__list-filter(label="фильтр" v-model="filterText")
+		template(#list)
+			data-page-list(:list="selectedList" @select="setCurrentTool" @intersected="onIntersect")
+		template(#card)
+			component(:is="selectedComponent" :item="currentTool" @save="save")
 
 </template>
 <script setup lang="ts">
@@ -34,46 +34,46 @@ const currentTool = ref(currentTab.value?.list[0])
 
 
 const selectedList = computed(() => {
-    return currentTab.value?.type === 'Material' ? currentTab.value?.list as Material[] : currentTab.value?.list as Tool[]
+	return currentTab.value?.type === 'Material' ? currentTab.value?.list as Material[] : currentTab.value?.list as Tool[]
 })
 const selectedComponent = computed(() => currentTab.value?.type === 'Material' ? MaterialComponent : ToolComponent)
 const setCurrentTool = (item: Material | Tool) => {
-    if (!item) return
-    const found = currentTab.value?.list.find(e => e.id === item.id)
-    found && (currentTool.value = found)
+	if (!item) return
+	const found = currentTab.value?.list.find(e => e.id === item.id)
+	found && (currentTool.value = found)
 }
 const save = async (ev: Material | Tool) => {
-    await currentTab.value?.save(ev)
-    const last = document.querySelector(".v-list > .v-list-item[data-last='true']")
-    last?.scrollIntoView()
-    currentTool.value = selectedList.value.at(-1) || currentTab.value?.list.at(-1)
+	await currentTab.value?.save(ev)
+	const last = document.querySelector(".v-list > .v-list-item[data-last='true']")
+	last?.scrollIntoView()
+	currentTool.value = selectedList.value.at(-1) || currentTab.value?.list.at(-1)
 }
 
 const onIntersect = (e: boolean) => {
-    e && currentTab.value?.newData()
+	e && currentTab.value?.newData()
 }
 
 
 const backgroundLoadingLists = () => {
-    switches.value.forEach(e => {
-        if (e.list.length) return
-        setTimeout(async () => await e.fetch())
-    })
+	switches.value.forEach(e => {
+		if (e.list.length) return
+		setTimeout(async () => await e.fetch())
+	})
 }
 backgroundLoadingLists()
 
 watch([route], async () => {
-    filterText.value = ""
-    !currentTab.value?.list.length && await currentTab.value?.fetch()
+	filterText.value = ""
+	!currentTab.value?.list.length && await currentTab.value?.fetch()
 })
 watch([currentTab], async () => {
-    currentTool.value = currentTab.value?.list[0]
-    console.log("currentTab", currentTab.value)
+	currentTool.value = currentTab.value?.list[0]
+	console.log("currentTab", currentTab.value)
 })
 
 watch([filterText], debounce(async () =>
-        await currentTab.value?.setFilter(filterText.value)
-    , 250))
+		await currentTab.value?.setFilter(filterText.value)
+	, 250))
 </script>
 
 
--- sfsb/src/components/supply-new/SupplyComponent.vue
+++ sfsb/src/components/supply-new/SupplyComponent.vue
@@ -1,26 +1,26 @@
 <template lang="pug">
-LayoutPage.supply(:background="'linear-gradient(45deg, #bbb, #efefef)'")
-    template(#title) Снабжение
-    template(#list)
-        v-list
-            v-list-item(@click="()=>{}" v-for="i in supplyMap(useSupplyStore)" :key="i.title" :active="i.path===path")
-                router-link.list-link(:to="i.path") {{ i.title }}
+	LayoutPage.supply(:background="'linear-gradient(45deg, rgb(var(--v-theme-surface)), rgb(var(--v-theme-surface-light))'")
+		template(#title) Снабжение
+		template(#list)
+			v-list
+				v-list-item(@click="()=>{}" v-for="i in supplyMap(useSupplyStore)" :key="i.title" :active="i.path===path")
+					router-link.list-link(:to="i.path") {{ i.title }}
 
-    template(#card)
-        suspended-component
-            router-view
+		template(#card)
+			suspended-component
+				router-view
 </template>
 <script setup lang="ts">
 
 import SuspendedComponent from "@/components/common/SuspendedComponent.vue"
-import { toRefs } from "vue";
+import {toRefs} from "vue";
 import LayoutPage from "@/components/common/LayoutPage.vue";
 import supplyMap from "./SupplyMap";
-import { useSupplyStore } from "@/pinia-store/supply";
-import { useRoute, useRouter } from "vue-router";
+import {useSupplyStore} from "@/pinia-store/supply";
+import {useRoute, useRouter} from "vue-router";
 
 const router = useRouter()
-const { path } = toRefs(useRoute())
+const {path} = toRefs(useRoute())
 const defaultRoute = "/supplier/all";
 
 path.value.split("/").at(-1) === 'supplier' && router.push(defaultRoute)
