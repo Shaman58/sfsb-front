9d542b965b2ef497fa521cbb716eef4c2da4b236
voskanvl <voskanvl@gmail.com>
2024-02-28 06:30:43 +0300

    main.ts: remove legacy

--- sfsb/src/main.ts
+++ sfsb/src/main.ts
@@ -1,37 +1,13 @@
 import App from "./App.vue";
-// import ErrorKeycloack from "./ErrorKeycloack.vue";
 import { createApp } from "vue";
 
 import { registerPlugins } from "@/plugins";
 import { registerDirectives } from "@/directives";
-// import keycloakService from "@/plugins/keycloak/service.mjs";
-// import FallbackApp from "./FallbackApp.vue";
 
 const app = createApp(App);
-// const errorPage = createApp(ErrorKeycloack);
-// const fallback = createApp(FallbackApp);
 
 registerPlugins(app)
 registerDirectives(app)
 
-// fallback.mount("#app");
-
-// keycloakService
-//     .init()
-//     .then(() => {
-//         fallback.unmount();
-//         registerPlugins(app);
-//         registerDirectives(app);
-//         app.mount("#app");
-//         console.log(keycloakService.getTocken());
-//     })
-//     .catch((error: unknown) => {
-//         fallback.unmount();
-//         console.log(error);
-//         errorPage.mount("#app");
-//     });
-
-// app.config.globalProperties.$keycloakService = keycloakService;
 app.config.globalProperties.$defaultColor = "#2F477E";
 app.mount("#app")
-console.log(import.meta.env.BASE_URL);
