477fe8855a299ca73b9da6fb00d1508b0e7eeb8b
voskanvl <voskanvl@gmail.com>
2024-02-28 06:45:04 +0300

    App: loader until user loaded

--- sfsb/src/App.vue
+++ sfsb/src/App.vue
@@ -1,7 +1,8 @@
 <template>
     <v-app>
         <navbar />
-        <router-view />
+        <v-progress-circular v-if="!user" indeterminate :color="$defaultColor" class="is-not-user" :size="62" :width="6"/>
+        <router-view v-else/>
     </v-app>
 </template>
 
@@ -11,12 +12,15 @@ import { useTheme } from "vuetify";
 import { watch } from "vue";
 import { useThemeStore } from "@/pinia-store/themeSwitcher"
 import { storeToRefs } from "pinia";
+import {useCurrentUserStore} from "@/pinia-store/currentUser";
 
 const { theme } = storeToRefs(useThemeStore())
 const { setDark, setLight } = useThemeStore()
 const themeGlobal = useTheme()
 const isLight = () => matchMedia("(prefers-color-scheme: light)").matches
 isLight() ? setLight() : setDark()
+
+const {user} = storeToRefs(useCurrentUserStore())
 
 watch(theme, () => {
     themeGlobal.global.name.value = theme.value ? "dark" : "light"
@@ -27,6 +31,12 @@ watch(theme, () => {
 <style lang="sass">
 .v-toolbar__content
     height: 32px !important
+
+.is-not-user
+    position: absolute
+    top: 50%
+    right: 50%
+    translate: 50% -50%
 
 [untouchable="true"]
     opacity: 0.5
