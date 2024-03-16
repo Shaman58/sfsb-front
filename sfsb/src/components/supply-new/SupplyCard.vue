3653cb1f847aa532b253952936b6c0fb6fb3db6f
voskanvl <voskanvl@gmail.com>
2024-03-16 13:59:33 +0300

    Supply: hot fix items-per-page-options

--- sfsb/src/components/supply-new/SupplyCard.vue
+++ sfsb/src/components/supply-new/SupplyCard.vue
@@ -10,6 +10,7 @@
                 no-data-text="Данные отсутствуют"
                 page-text=""
                 :show-current-page="true"
+                :items-per-page-options="itemsPerPageOptions"
                 v-if="width > breakPoint"
             )
 
@@ -115,6 +116,14 @@ const headers = [
     {title: "Плотность", value: "density"},
     {title: "Стоимость", value: "price.amount", key: "price.amount"},
     {title: "Сохранить", value: "editing"}
+]
+
+const itemsPerPageOptions = [
+    {value: 10, title: '10'},
+    {value: 20, title: '20'},
+    {value: 50, title: '50'},
+    {value: 100, title: '100'},
+    {value: -1, title: 'Все'}
 ]
 
 
