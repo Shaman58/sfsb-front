c90f4a228a2cb179bcd9595889cb055a66420ba6
voskanvl <voskanvl@gmail.com>
2024-03-18 13:46:32 +0300

    Theme: fixe default value in store

--- sfsb/src/pinia-store/themeSwitcher.ts
+++ sfsb/src/pinia-store/themeSwitcher.ts
@@ -3,7 +3,7 @@ import {  ref } from "vue";
 
 
 export const useThemeStore = defineStore("theme", () => {
-    const theme = ref<boolean>(false);
+    const theme = ref<boolean>(window.localStorage.getItem("theme")==="dark");
 
     const setLight = () => {
         theme.value = false;
