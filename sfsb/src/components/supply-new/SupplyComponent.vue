c2feaabab86f2a8c40e476dd6badb3c30427e80b
voskanvl <voskanvl@gmail.com>
2024-06-16 15:59:58 +0300

    Supply: fix click category behavior

--- sfsb/src/components/supply-new/SupplyComponent.vue
+++ sfsb/src/components/supply-new/SupplyComponent.vue
@@ -3,7 +3,7 @@
         template(#title) Снабжение
         template(#list)
             v-list
-                v-list-item(@click="()=>{}" v-for="i in supplyMap(useSupplyStore)" :key="i.title" :active="i.path===path")
+                v-list-item.supply__link-container(@click="()=>{}" v-for="i in supplyMap(useSupplyStore)" :key="i.title" :active="i.path===path")
                     router-link.list-link(:to="i.path")
                         .supply__link
                             span {{ i.title }}
@@ -14,35 +14,41 @@
                 router-view
 </template>
 <script setup lang="ts">
-
-import SuspendedComponent from "@/components/common/SuspendedComponent.vue"
-import {toRefs} from "vue";
+import SuspendedComponent from "@/components/common/SuspendedComponent.vue";
+import { toRefs } from "vue";
 import LayoutPage from "@/components/common/LayoutPage.vue";
 import supplyMap from "./SupplyMap";
-import {useSupplyStore} from "@/pinia-store/supply";
-import {useRoute, useRouter} from "vue-router";
-import {storeToRefs} from "pinia";
+import { useSupplyStore } from "@/pinia-store/supply";
+import { useRoute, useRouter } from "vue-router";
 
-const router = useRouter()
-const {path} = toRefs(useRoute())
+const router = useRouter();
+const { path } = toRefs(useRoute());
 const defaultRoute = "/supplier/all";
 
-
 const isNeedToRedirect = (): boolean => {
-    return path.value.split("/").at(-1) === 'supplier'
-        || path.value.split("/").at(-2) === 'supplier'
-        && path.value.split("/").at(-1) === ''
-}
+    return (
+        path.value.split("/").at(-1) === "supplier" ||
+        (path.value.split("/").at(-2) === "supplier" &&
+            path.value.split("/").at(-1) === "")
+    );
+};
 
-isNeedToRedirect() && router.push(defaultRoute)
-
+isNeedToRedirect() && router.push(defaultRoute);
 </script>
 <style scoped lang="sass">
 .supply
+    &__link-container
+        position: relative
+
     &__link
         display: flex
         align-items: center
         justify-content: space-between
+
+        &::before
+            content: ""
+            position: absolute
+            inset: 0
 
     &__link-badge
         display: grid
