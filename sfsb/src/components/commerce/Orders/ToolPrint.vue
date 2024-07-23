480bb99ad5c06daec0e27069a96e232b1319c6bb
voskanvl <voskanvl@gmail.com>
2024-07-23 17:03:56 +0300

    OrderCard handle ToolPrint exception

--- sfsb/src/components/commerce/Orders/ToolPrint.vue
+++ sfsb/src/components/commerce/Orders/ToolPrint.vue
@@ -1,37 +1,54 @@
 <template lang="pug" src="./ToolPrint.pug"></template>
 
 <script setup lang="ts">
-
-import {useRoute} from "vue-router";
-import {useOrdersStore} from "@/pinia-store/orders";
-import {computed, ref, watch, watchEffect} from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { useOrdersStore } from "@/pinia-store/orders";
+import { computed, ref, watch, watchEffect } from "vue";
 import ToolPrint from "@/components/commerce/Orders/ToolPrint.vue";
-import router from "@/router";
 
-const route = useRoute()
-const {getToolPrint} = useOrdersStore()
+const route = useRoute();
+const router = useRouter();
+const { getToolPrint } = useOrdersStore();
 const toolPrint = ref<ToolPrint | null>(null);
 const totalAmount = computed(() => {
-    return toolPrint.value && toolPrint.value.tools.reduce((acc, cur) => acc += cur.amount, 0) || 0
-})
+    return (
+        (toolPrint.value &&
+            toolPrint.value.tools.reduce(
+                (acc, cur) => (acc += cur.amount),
+                0
+            )) ||
+        0
+    );
+});
 const totalPrice = computed(() => {
-    return toolPrint.value && toolPrint.value.tools.reduce((acc, cur) => acc += cur.itemPrice, 0) || 0
-})
+    return (
+        (toolPrint.value &&
+            toolPrint.value.tools.reduce(
+                (acc, cur) => (acc += cur.itemPrice),
+                0
+            )) ||
+        0
+    );
+});
 
 const close = () => {
-    router.back()
-}
+    router.back();
+};
 
-const print = window.print
+const print = window.print;
 
-watch([toolPrint], () => setTimeout(print, 1000))
+watch([toolPrint], () => setTimeout(print, 1000));
 
 watchEffect(async () => {
-    const res = await getToolPrint(+route.params.id, +route.params.company);
-    toolPrint.value = res || null
-})
+    try {
+        const res = await getToolPrint(+route.params.id, +route.params.company);
+        if (!res) throw Error("error");
+        toolPrint.value = res || null;
+    } catch (err) {
+        await router.push(`/commerce/orders/${+route.params.id}`);
+    }
+});
 </script>
-
 
 <style scoped lang="sass">
 @import "./ToolPrint"
