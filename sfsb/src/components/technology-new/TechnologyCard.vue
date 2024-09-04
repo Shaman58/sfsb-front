710c708e73f0bdbae8c0f45039eab56445e7839b
voskanvl <voskanvl@gmail.com>
2024-09-04 12:34:15 +0300

    TechnologyCard: change behavior

--- sfsb/src/components/technology-new/TechnologyCard.vue
+++ sfsb/src/components/technology-new/TechnologyCard.vue
@@ -10,38 +10,46 @@
 
 </template>
 <script setup lang="ts">
-import {computed, toRefs} from "vue";
-import {useRoute, useRouter} from "vue-router";
-import {storeToRefs} from "pinia";
-import {useOrdersStore} from "@/pinia-store/orders";
+import { ref, toRefs, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { storeToRefs } from "pinia";
+import { useOrdersStore } from "@/pinia-store/orders";
 import LayoutMain from "@/components/common/LayoutMain.vue";
 import TechnologyTable from "@/components/technology-new/TechnologyTable.vue";
 import TechnologySelf from "@/components/technology-new/TechnologySelf.vue";
 import SuspendedComponent from "@/components/common/SuspendedComponent.vue";
 import TechnologyCreateDialog from "@/components/technology/TechnologyCreateDialog.vue";
-import {useTechnologyStore} from "@/pinia-store/technology";
+import { useTechnologyStore } from "@/pinia-store/technology";
 
-const router = useRouter()
-const {params, path} = toRefs(useRoute())
+const router = useRouter();
+const { params, path } = toRefs(useRoute());
 
-const {orders, loading: loadingOrders} = storeToRefs(useOrdersStore())
-const {getOrders} = useOrdersStore()
-!orders.value.length && await getOrders()
+const { orders, loading: loadingOrders } = storeToRefs(useOrdersStore());
+const { getOrders, getOrderById } = useOrdersStore();
+!orders.value.length && (await getOrders());
 
-const hasCurrentOrder = orders.value.find(e => e.id + "" === params.value.id)
-if (!hasCurrentOrder) router.push("/not-found")
+const hasCurrentOrder = orders.value.find((e) => e.id === +params.value.id);
+!hasCurrentOrder && getOrderById(+params.value.id);
 
-const order = computed(() => orders.value.find(e => e.id + "" === params.value.id) || orders.value[0])
-console.log(order.value)
+// const order = computed(
+//     () => orders.value.find((e) => e.id === +params.value.id) || orders.value[0]
+// );
+const order = ref(orders.value[0]);
+console.log(order.value);
 
-const {dialogVisible, currentItem} = storeToRefs(useTechnologyStore())
-const {setCurrentItem} = useTechnologyStore()
+const { dialogVisible, currentItem } = storeToRefs(useTechnologyStore());
+const { setCurrentItem } = useTechnologyStore();
 
-const refresh = async () => {
-}
+const refresh = async () => {};
 
+watch(
+    [params],
+    async () => {
+        order.value = await getOrderById(+params.value.id);
+    },
+    { immediate: true }
+);
 </script>
-
 
 <style scoped lang="sass">
 .technology-card
@@ -62,5 +70,4 @@ const refresh = async () => {
             width: 4px
             background-color: var(--scroll-color)
             border-radius: 8px
-
 </style>
