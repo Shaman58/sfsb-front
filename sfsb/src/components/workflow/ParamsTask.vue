2059c69eb37cc1d60162a8ab26337cc5a896a849
voskanvl <voskanvl@gmail.com>
2024-09-29 15:28:59 +0300

    Workflow: continue

---
+++ sfsb/src/components/workflow/ParamsTask.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+    v-menu.menu(
+        v-model="menu"
+        :close-on-content-click="false"
+        activator="parent"
+        location="end"
+        scroll-strategy="reposition"
+        min-width="300px"
+    )
+        v-card.pa-2()
+            h3.menu__header {{task.name}}
+            v-form
+                v-text-field(label="Описание" v-model.lazy="task.description")
+                SetTime(v-model:start-at="localTask.startAt" v-model:end-at="localTask.endAt")
+                RepalceToResource(v-model:resourceId="localTask.workflowId")
+            .d-flex.justify-center
+                v-btn-group
+                    v-btn.flex-grow-1(color="orange-darken-1" @click="onTaskChange") Изменить
+                    v-btn.flex-grow-1(color="grey-lighten-3" @click="menu=false") Отмена
+</template>
+
+<script setup lang="ts">
+import { type ModelRef, ref, toRefs, watch } from "vue";
+import SetTime from "@/components/workflow/SetTime.vue";
+import RepalceToResource from "@/components/workflow/RepalceToResource.vue";
+
+const menu = defineModel<boolean>("menu");
+const task: ModelRef<Task, string> = defineModel<Task>("task", {
+    required: true,
+});
+const emit = defineEmits(["change"]);
+
+const { startAt, endAt } = toRefs(task.value);
+const localTask = ref<Task>({ ...task.value } as Task);
+
+const onTaskChange = () => {
+    emit("change", localTask.value);
+    menu.value = false;
+};
+
+watch([startAt, endAt], () => {
+    console.log("время изменилось");
+    localTask.value = { ...task.value };
+});
+</script>
+
+<style scoped lang="sass">
+.menu
+    &__header
+        text-align: center
+</style>
--- sfsb/src/components/workflow/TaskComponent.vue
+++ sfsb/src/components/workflow/TaskComponent.vue
@@ -4,20 +4,39 @@
         :draggable="canDraggable"
         @dragend="onDragEnd"
         @dragstart="onDragStart"
+        @contextmenu.prevent.stop="menu=true"
         :style="{width: duration + 'px', left: left + 'px', boxShadow}"
     )
         .task__border.task__border_left(@mousedown.prevent="selectBorder($event,'left')")
         .task__caption
             h4 {{props.task.name}}
-            div {{console}}
+            div {{consoleText}}
             p {{props.task.description}}
         .task__border.task__border_right(@mousedown.prevent="selectBorder($event,'right')")
+
+        v-tooltip(
+            activator="parent"
+            location="bottom"
+        )
+            h3 {{task.name}}
+            p {{task.description}}
+            div Время начала:
+                time {{new Date(task.startAt).toLocaleDateString()}}&nbsp;
+                time
+                    strong {{new Date(task.startAt).toLocaleTimeString()}}
+            div Время завершения:
+                time {{new Date(task.endAt).toLocaleDateString()}}&nbsp;
+                time
+                    strong {{new Date(task.endAt).toLocaleTimeString()}}
+
+        ParamsTask(v-model:menu="menu" v-model:task="props.task" @change="onChange($event)")
 </template>
 <script setup lang="ts">
-import { computed, inject, ref, type Ref, toRefs } from "vue";
+import { computed, inject, ref, type Ref, toRefs, watch } from "vue";
 import { storeToRefs } from "pinia";
 import useTaskMoving from "@/pinia-store/taskMoving";
 import { useWorkflow } from "@/pinia-store/workflow";
+import ParamsTask from "@/components/workflow/ParamsTask.vue";
 
 const props = defineProps<{ task: Task; active: boolean }>();
 const { startAt, endAt, color } = toRefs(props.task);
@@ -25,8 +44,11 @@ const { startAt, endAt, color } = toRefs(props.task);
 const scale = inject<Ref<number>>("scale");
 const canDraggable = ref(true);
 
+const menu = ref(false); // Показывать меню
+
 const { taskMoving, borderMoving } = storeToRefs(useTaskMoving());
 const { getFirstTask } = storeToRefs(useWorkflow());
+const { relocateTask } = useWorkflow();
 
 const startDate = new Date(getFirstTask.value.startAt).setHours(0, 0, 0, 0);
 
@@ -35,7 +57,7 @@ const boxShadow = computed(
     () => `0 0 ${props.active ? "18px" : "0"} ${color.value}`
 );
 
-const console = ref("");
+const consoleText = ref("");
 
 defineExpose({ element, id: props.task.id });
 const { scrollBody } = storeToRefs(useTaskMoving());
@@ -70,6 +92,20 @@ const selectBorder = (event: MouseEvent, border: "left" | "right") => {
         x: event.x + scrollBody.value,
     };
 };
+
+const onChange = (newValue: Task) => {
+    console.log("data to change", newValue);
+    relocateTask(newValue.id, newValue.workflowId, {
+        startAt: newValue.startAt,
+        endAt: newValue.endAt,
+    });
+};
+watch([props.task], () => {
+    console.log("props.task", props.task);
+});
+watch([left], () => {
+    console.log("left", left.value);
+});
 </script>
 
 <style scoped lang="sass">
